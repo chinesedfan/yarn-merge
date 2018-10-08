a097ecd2a95d6d55c8eb8fbca3e1917df50528ef
Xianming Zhong <chinesedfan@qq.com>
2018-10-08 22:56:07 +0800

    parse file to dep items

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,6 @@
+// @flow
+import { parse } from '@yarnpkg/lockfile';
+
 type PkgWithScope = string; // pkg@scope
 type VersionScope = string;
 type FileParseItem = {
@@ -8,6 +11,13 @@ type FileParseItem = {
 type FileParseResult = {
     type: string,
     object: {[key: PkgWithScope]: FileParseItem}
+};
+
+type DepItem = {
+    ...FileParseItem,
+    id: PkgWithScope,
+    name: string,
+    parent?: DepItem
 };
 
 type GraphInput = {
@@ -22,7 +32,31 @@ type GraphInput = {
     }
 };
 
-function parseFile(content: string): FileParseResult {
+function parseFile(content: string): Array<DepItem> {
+    const directDeps: Array<DepItem> = [];
+    const result: FileParseResult = parse(content);
+    const parents: {[key: PkgWithScope]: FileParseItem} = {};
+
+    Object.keys(result.object).forEach((pkgWithScope) => {
+        const item = result.object[pkgWithScope];
+        if (item.dependencies) {
+            for (const pkg in item.dependencies) {
+                parents[`${pkg}@${item.dependencies[pkg]}`] = item;
+            }
+        }
+
+        const dep = {
+            ...item,
+            id: pkgWithScope,
+            name: pkgWithScope.substr(0, pkgWithScope.lastIndexOf('@')),
+            parent: parents[pkgWithScope]
+        };
+        if (!dep.parent) {
+            directDeps.push(dep);
+        }
+    });
+
+    return directDeps;
 }
 
 function drawSamePkg(pkg: string, result: FileParseResult): GraphInput {
